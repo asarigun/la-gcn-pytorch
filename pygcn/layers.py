ff86809d1c2f3758d39c1b2d4528c00d9465b128
Ahmet Sarigun <47702994+asarigun@users.noreply.github.com>
2021-02-15 21:50:53 +0300

    Update layers.py

--- pygcn/layers.py
+++ pygcn/layers.py
@@ -15,6 +15,7 @@ class GraphConvolution(Module):
         super(GraphConvolution, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
+        self.add_all = add_all
         self.weight = Parameter(torch.FloatTensor(in_features, out_features))
         if bias:
             self.bias = Parameter(torch.FloatTensor(out_features))
@@ -39,7 +40,7 @@ class GraphConvolution(Module):
         input_new = []
         for i in range(len(self.add_all)):
             aa = torch.gather(input, [i])
-            aa_tile = torch.tile(aa, [len(self.add_all[i]), 1) #expand central
+            aa_tile = torch.tile(aa, [len(self.add_all[i]), 1]) #expand central
             bb_nei = torch.gather(input,self.add_all[i])
             cen_nei = torch.cat([aa_tile, bb_nei],1)
                                       
