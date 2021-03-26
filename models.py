f28f8b680f785975126119eb49272d8f6efb7d6a
Ahmet Sarigun <47702994+asarigun@users.noreply.github.com>
2021-03-26 11:58:33 +0300

    Update models.py

--- models.py
+++ models.py
@@ -7,8 +7,7 @@ class GCN_MASK(nn.Module):
     def __init__(self, add_all, nfeat, nhid, nclass, dropout):
         super(GCN_MASK, self).__init__()
         self.add_all = add_all
-        #self.mask = []
-        self.gc1 = GraphConvolution(nfeat, nhid) #add_all = self.add_all
+        self.gc1 = GraphConvolution(nfeat, nhid) 
         self.gc2 = gcnmask(self.add_all, nhid, nclass)
         self.dropout = dropout
         
