dd3762bb13776755c5ecce0d7d203e60bd081722
Ahmet Sarigun <47702994+asarigun@users.noreply.github.com>
2021-03-31 21:36:17 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,6 +9,35 @@ from scipy.sparse.linalg.eigen.arpack import eigsh
 import sys
 import pdb
 import torch
+"""
+ind.dataset_str.x => the feature vectors of the training instances as scipy.sparse.csr.csr_matrix object;
+ind.dataset_str.tx => the feature vectors of the test instances as scipy.sparse.csr.csr_matrix object;
+ind.dataset_str.allx => the feature vectors of both labeled and unlabeled training instances 
+    (a superset of ind.dataset_str.x) as scipy.sparse.csr.csr_matrix object;
+    
+ind.dataset_str.y => the one-hot labels of the labeled training instances as numpy.ndarray object;
+ind.dataset_str.ty => the one-hot labels of the test instances as numpy.ndarray object;
+ind.dataset_str.ally => the labels for instances in ind.dataset_str.allx as numpy.ndarray object;
+
+ind.dataset_str.graph => a dict in the format {index: [index_of_neighbor_nodes]} as collections.defaultdict object;
+ind.dataset_str.test.index => the indices of test instances in graph, for the inductive setting as list object.
+
+All objects above must be saved using python pickle module.
+    
+To cora example:
+ ind.dataset_str.x => eigenvectors of training examples, a class object scipy.sparse.csr.csr_matrix, shape: (140, 1433)
+ ind.dataset_str.tx => eigenvector test case, shape: (1000, 1433)
+ ind.dataset_str.allx => + None None tagged training examples tag feature vector is ind.dataset_str.x superset, shape: (1708, 1433)
+
+ ind.dataset_str.y => tag of training examples, hot encoded, numpy.ndarray class instance, the object is numpy.ndarray, shape: (140, 7)
+ ind.dataset_str.ty => tag test examples, hot encoded, numpy.ndarray class instance, shape: (1000, 7)
+ ind.dataset_str.ally => ind.dataset_str.allx corresponding label, one-hot encoding, shape: (1708, 7)
+
+ ind.dataset_str.graph => map data, collections.defaultdict class instance, the format {index: [index_of_neighbor_nodes]}
+ ind.dataset_str.test.index => test case id, 2157 OK
+
+ These documents must be stored with the pickle module of python
+"""
 
 def parse_index_file(filename):
     """Parse index file."""
